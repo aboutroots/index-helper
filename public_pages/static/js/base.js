61ad4c1299bbba3e33106cfc580deb748ad71406
aboutroots <popiel.mw@gmail.com>
2019-02-13 11:04:35 +0100

    Add spinner and file extension warning

--- public_pages/static/js/base.js
+++ public_pages/static/js/base.js
@@ -22,13 +22,31 @@ if (fileInput) {
 const runBtn = document.querySelector("#run-icon");
 const fileForm = document.querySelector("#file-form");
 const respBox = document.querySelector("#resp-box");
-// const downloadLink = document.querySelector("#download-wrapper a");
-// const infoBox = document.querySelector("#info-box");
-// downloadWrapper.style.transition = "all 0.5s";
-// infoBox.style.transition = "all 0.5s";
 
-runBtn.addEventListener("click", () => {
+async function loadSpinner() {
+  respBox.innerHTML = `
+    <div class="spinner">
+      <div class="bounce1"></div>
+      <div class="bounce2"></div>
+      <div class="bounce3"></div>
+    </div>
+  `;
+  respBox.classList.add("spinner-wrap");
+  respBox.classList.add("expanded");
+}
+
+function handleForm() {
   if (fileInput.files.length) {
+    // check if file is a txt file
+    const extension = fileInput.files[0].name.split(".").pop();
+    if (extension !== "txt") {
+      respBox.textContent =
+        'Please make sure to select a simple text file (with ".txt" extension)';
+      respBox.classList.add("expanded");
+      return;
+    }
+
+    loadSpinner();
     const formData = new FormData(fileForm);
     const options = {
       method: "POST",
@@ -37,35 +55,39 @@ runBtn.addEventListener("click", () => {
     fetch("", options)
       .then(resp => resp.json())
       .then(json => {
+        respBox.classList.remove("spinner-wrap");
         if (json.success) {
+          respBox.innerHTML = `<a href="${
+            json.uploaded_file_url
+          }" download="index_new.txt"><i class="fa fa-download"> <span>index_new.txt</span></i></a>`;
           respBox.classList.add("expanded");
-          respBox.innerHTML = `<a href="${json.uploaded_file_url}" download="index_new.txt"><i class="fa fa-download"> <span>index_new.txt</span></i></a>`;
         } else {
           console.log(json.error);
           respBox.innerHTML = json.error;
           respBox.classList.add("expanded");
-          // downloadWrapper.classList.remove("expanded");
         }
       })
       .catch(reason => {
+        respBox.classList.remove("spinner-wrap");
         console.log(`Promise rejected with reason: ${reason}`);
         respBox.textContent =
           "Some strange error occurred. Please contact let me (aboutroots) know about it!";
         respBox.classList.add("expanded");
-        // downloadWrapper.classList.remove("expanded");
       });
   }
-});
+}
+runBtn.addEventListener("click", handleForm);
 
 //copy problematic line to clipboard on click
 respBox.addEventListener("click", () => {
   let range = document.createRange();
   let badLine = document.getElementById("bad-line");
-  if (!badLine) return
+  if (!badLine) return;
   range.selectNode(badLine);
   window.getSelection().removeAllRanges();
   window.getSelection().addRange(range);
   document.execCommand("copy");
 });
+
 // hide warning message on click
 respBox.addEventListener("click", () => respBox.classList.remove("expanded"));
